12b7fc1c536ecd789f27db37aaf7e780a5803eb8
Måns Edenfalk <Medenfalk@gmail.com>
2022-12-21 11:05:41 +0100

    added modal

---
+++ src/eventlisteners.ts
@@ -0,0 +1,7 @@
+export const infoBtnEl = document.querySelector<HTMLButtonElement>('#info')!
+
+infoBtnEl.addEventListener('click', async e => {
+    const target = e.target as HTMLButtonElement
+    console.log(target);
+    
+})
