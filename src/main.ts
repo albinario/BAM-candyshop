29c78ebe84837832f20cd7f8e80f31cf07a898f4
Måns Edenfalk <Medenfalk@gmail.com>
2022-12-21 14:31:42 +0100

    view modal done

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import './style.css'
 import { apiUrl, getCandys } from './api'
 import { Candy } from './types'
@@ -18,8 +19,28 @@ mainEl.innerHTML = candysArr.map(candy => `
 				<p class="card-title">${candy.name}</p>
 				<p class="card-text">${candy.price} sek</p>
 				<div class="d-flex justify-content-between">
-					<button href="#" class="btn btn-warning" aria-label="view-candy"><i class="fa-regular fa-eye"></i><span class="d-none d-sm-inline"> View</span></button>
+					<button class="btn btn-warning" aria-label="view-candy" type="button" data-bs-toggle="modal" data-bs-target="#view-${candy.id}"><i class="fa-regular fa-eye"></i><span class="d-none d-sm-inline"> View</span></button>
 					<button href="#" class="btn btn-success" aria-label="buy-candy"><i class="fa-solid fa-plus"></i> Buy</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="modal fade" id="view-${candy.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<p class="modal-title fs-5" id="exampleModalLabel">${candy.name}</p>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<img src="${apiUrl}/${candy.images.large}" alt="${candy.name}">
+					${candy.description}
+					<p>${candy.price} sek</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-success" aria-label="buy-candy"><i class="fa-solid fa-plus"></i> Buy</button>
 				</div>
 			</div>
 		</div>
