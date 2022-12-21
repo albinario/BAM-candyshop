5de616f4a3419c5aca4fff40496a0101bdc85ce5
Måns Edenfalk <Medenfalk@gmail.com>
2022-12-21 13:38:55 +0100

    popup test

--- src/eventlisteners.ts
+++ src/eventlisteners.ts
@@ -1,7 +1,7 @@
-export const infoBtnEl = document.querySelector<HTMLButtonElement>('#info')!
+// const infoBtnEl = document.querySelector<HTMLButtonElement>('#info')!
 
-infoBtnEl.addEventListener('click', async e => {
-    const target = e.target as HTMLButtonElement
-    console.log(target);
+// infoBtnEl.addEventListener('click', async e => {
+//     const target = e.target as HTMLButtonElement
+//     console.log(target);
     
-})
+// })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './style.css'
 import { apiUrl, getCandys } from './api'
 import { Candy } from './types'
 import { shuffleArray } from './functions'
+//import { infoBtnEl } from './eventlisteners'
 
 const candys = await getCandys()
 const candysArr: Candy[] = candys.data
@@ -17,8 +18,18 @@ mainEl.innerHTML = candysArr.map(candy => `
 			<div class="card-body">
 				<p class="card-title">${candy.name}</p>
 				<p class="card-text">${candy.price} sek</p>
-				<div class="d-flex justify-content-between">
-					<button href="#" class="btn btn-warning" aria-label="view-candy"><i class="fa-regular fa-eye"></i><span class="d-none d-sm-inline"> View</span></button>
+				<div id="info-${candy.id}" class="d-flex justify-content-between">
+					<button id="info" href="#" class="btn btn-warning" aria-label="view-candy"><i class="fa-regular fa-eye"></i><span class="d-none d-sm-inline"> View</span></button>
+					<div class ="popup-wrapper d-none">
+						<div class="popup">
+							<div class="popup-close">X</div>
+							<div class="popup-content">
+								<h2>${candy.name}</h2>
+								<p>${candy.description}</p>
+								<p>${candy.price} SEK</p>
+							</div>	
+						</div>
+					</div>	
 					<button href="#" class="btn btn-success" aria-label="buy-candy"><i class="fa-solid fa-plus"></i> Buy</button>
 				</div>
 			</div>
@@ -26,3 +37,15 @@ mainEl.innerHTML = candysArr.map(candy => `
 	</div>
 `)
 .join('')
+
+
+
+
+candysArr.forEach(candy => {
+	document.querySelector(`#info-${candy.id}`)?.addEventListener('click', () => {
+		console.log("clickclick")
+
+
+
+	})
+})
