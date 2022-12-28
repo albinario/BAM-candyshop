d4c3c6f2d13a50da46fe603362cfdaea3aa4e807
Albin Lindeborg <albin.lindeborg@gmail.com>
2022-12-28 14:34:14 +0100

    compress table

--- src/functions.ts
+++ src/functions.ts
@@ -2,8 +2,6 @@ import { Candy } from './types'
 import { inCartEls, tableContentEl  } from './elements'
 import { apiUrl } from './api'
 
-
-
 export const shuffleArray = (array: Candy[]) => {
 	for (let i = array.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
@@ -21,19 +19,17 @@ export const updateCart = (candysInCart: Candy[], candy: Candy) => {
 }
 
 export const renderCandyInCart = (candy: Candy) => {
-	tableContentEl.innerHTML += 
+	tableContentEl.innerHTML += `
+		<tr scope="row">
+			<td><img src="${apiUrl}/${candy.images.thumbnail}" class="card" alt="${candy.name}"></td>
+			<td>${candy.name}</td>
+			<td class="text-nowrap">
+				<span class="badge bg-danger">-</span>
+				<span class="badge bg-warning">1</span>
+				<span class="badge bg-success">+</span>
+			</td>
+			<td class="text-center text-nowrap">${candy.price} kr</td>
+			<td class="text-center text-nowrap">${candy.price} kr</td>
+		</tr>
 	`
-		<tr>
-		<td><img src="${apiUrl}/${candy.images.thumbnail}" class="" alt="${candy.name}"></td>
-		<td>${candy.name}</td>
-		<td>
-			<span class="badge bg-danger">-</span>
-			<span class="badge bg-warning">0</span>
-			<span class="badge bg-success">+</span>
-		</td>
-		<td>${candy.price} sek</td>
-		<td>${candy.price} sek</td>
-		</tr>	
-	`	
 }
-
