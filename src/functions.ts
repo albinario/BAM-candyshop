708aa1035cfe7bdd15d1066e0785cd0814d81ed2
Måns Edenfalk <Medenfalk@gmail.com>
2022-12-28 11:25:01 +0100

    add styles to shopping cart popup

--- src/functions.ts
+++ src/functions.ts
@@ -20,12 +20,13 @@ export const updateCart = (candysInCart: Candy[], candy: Candy) => {
 	tableContentEl.innerHTML += 
 `
 			<tr>
-			<th scope="row"><img src="${apiUrl}/${candy.images.thumbnail}" class="card-img-top cart-img" alt="${candy.name}">
-			<span>${candy.name}</span></th>
+			<td><img src="${apiUrl}/${candy.images.thumbnail}" class="" alt="${candy.name}"></td>
+			<td>${candy.name}</td>
 			<td>
 				<span class="badge bg-danger">-</span>
 				<span class="badge bg-warning">0</span>
-				<span class="badge bg-success">+</span></td>
+				<span class="badge bg-success">+</span>
+			</td>
 			<td>${candy.price} sek</td>
 			<td>${candy.price} sek</td>
 			</tr>	
